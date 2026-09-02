2538f6e8fff3210ece99a931e629cfdace912613
Dhanush F G <dhanush@coreforgeindia.com>
2026-09-02 15:49:52 +0530

    feat: implement services page with categorized service offerings and navigation links

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,5 @@
 import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 import SEO from '../components/SEO'
 import PageHero from '../components/PageHero'
 import WorkflowSection from '../sections/WorkflowSection'
